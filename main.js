fb3d6bf7338f8ef8646dca009a9a2ff35f01880c
Felipe Merchan <felipe-sc@hotmail.es>
2022-10-09 17:28:15 -0500

    Consultas a la API para escribir HTML dinámico

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY='live_JcUEyGccUMLYJ3Bn3IoCVEl4PBDodEyGSc4lu90HKcA4EYdK4KQuT0EgDZtRW4yq'
 const BASE_URL = 'https://api.thecatapi.com/v1/'
 const API_URL_RANDOM=`${BASE_URL}images/search?limit=2&api_key=${API_KEY}`
-const API_URL_FAVORITES=`${BASE_URL}favourites?limit=2&api_key=${API_KEY}`
+const API_URL_FAVORITES=`${BASE_URL}favourites?api_key=${API_KEY}`
 
 const $spanError = document.getElementById('error')
 
@@ -13,11 +13,16 @@ async function loadRandomCats() {
     if (response.status !== 200 ) {
         $spanError.innerHTML = `Hubo un error ${response.status} ${data.message}`
     } else {
-        const img1 = document.getElementById('img1')
-        const img2 = document.getElementById('img2')
+        const $img1 = document.getElementById('img1')
+        const $img2 = document.getElementById('img2')
+        const $button1 = document.getElementById('btn1')
+        const $button2 = document.getElementById('btn2')
 
-        img1.src = data[0].url
-        img2.src = data[1].url
+        $img1.src = data[0].url
+        $img2.src = data[1].url
+
+        $button1.onclick = () => saveFavoriteCat(data[0].id)
+        $button2.onclick = () => saveFavoriteCat(data[1].id)
     }
 }
 
@@ -28,21 +33,37 @@ async function loadFavoriteCats() {
 
     if (response.status !== 200 ) {
         $spanError.innerHTML = `Hubo un error ${response.status} ${data.message}`
+    } else {
+        const $section = document.getElementById('favotireMichis')
+        data.forEach(cat => {
+            const $article = document.createElement('article')
+            const $img = document.createElement('img')
+            const $button = document.createElement('button')
+            const btnText = document.createTextNode('Eliminar michi de los favoritos')
+            $button.appendChild(btnText)
+            $img.src = cat.image.url
+            $img.width = 150
+
+            $article.appendChild($img)
+            $article.appendChild($button)
+            $section.appendChild($article)
+        }) 
     }
 }
 
-async function saveFavoriteCat() {
+async function saveFavoriteCat(id) {
     const response = await fetch(API_URL_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            image_id: 'dje',
+            image_id: id,
         }),
     })
     const data = await response.json()
     console.log('saveFavoriteCat', response)
+
     if (response.status !== 200 ) {
         $spanError.innerHTML = `Hubo un error ${response.status} ${data.message}`
     }
